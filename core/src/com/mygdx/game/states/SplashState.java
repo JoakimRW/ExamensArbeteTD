b2dba9dd01f5e47c8ba330ab29212c3a2b6c8420
JoakimRW <rtuu41@gmail.com>
2017-02-28 14:55:04 +0100

    Bytte bakgrundsfärg så man kan se att det funkar

--- core/src/com/mygdx/game/states/SplashState.java
+++ core/src/com/mygdx/game/states/SplashState.java
@@ -18,7 +18,7 @@ public class SplashState extends GameState {
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+		Gdx.gl.glClearColor(0f, 1f, 1f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 
