1ce94408bcf3b9c0f9f6c38e0ff89219dc720416
JoakimRW <rtuu41@gmail.com>
2017-03-01 21:23:52 +0100

    fixade mainmenuknapparna när du ändrar upplösning

--- core/src/com/mygdx/game/stages/MainMenuStage.java
+++ core/src/com/mygdx/game/stages/MainMenuStage.java
@@ -20,13 +20,13 @@ public class MainMenuStage extends Stage {
 	public MainMenuStage(final GameStateManager gsm) {
 		initMainMenu(gsm);
 	}
-	private void initMainMenu(final GameStateManager gsm){
+
+	private void initMainMenu(final GameStateManager gsm) {
 		skin = new Skin(Gdx.files.internal("MainMenuSkin.json"));
 		Table table = new Table();
 		final TextButton button = new TextButton("New Game", skin, "default");
 		final TextButton exitButton = new TextButton("Quit Game", skin, "default");
 		button.setSize(200f, 50f);
-
 
 		button.addListener(new ClickListener() {
 
@@ -36,39 +36,43 @@ public class MainMenuStage extends Stage {
 			}
 
 		});
-		
+
 		exitButton.addListener(new ClickListener() {
 
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				Gdx.app.exit();
 			}
-			
+
 		});
-		
+
 		table.setWidth(this.getWidth());
-		table.align(Align.center|Align.top);
+		table.align(Align.center | Align.top);
 		table.setPosition(0, Gdx.graphics.getHeight());
 		table.padTop(30);
 		table.add(button).padBottom(30f);
 		table.row();
 		table.add(exitButton);
-		
+
 		this.addActor(table);
 	}
 
 	@Override
+	public void act() {
+		super.act();
+	}
+
+	@Override
 	public void draw() {
+		
 		SpriteBatch batch = new SpriteBatch();
 		Sprite sprite = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
 		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
-		
+		this.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		super.draw();
 	}
-	
-	
 
 }
