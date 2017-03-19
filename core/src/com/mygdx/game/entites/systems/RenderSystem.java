922a188b3a55717d999b70fc557ae611d7a80e2b
michael sjögren <mikel-carl@hotmail.com>
2017-03-19 17:16:35 +0100

    tinted colors on healbar depending of health
    added diffrent color tints to the healthbar depending of the percentage
    of current hp
    healthbar is now a sprite
    - green 100% or less
    - yellow 75% or less
    - orange 50% or less
    - red 25% or less

--- core/src/com/mygdx/game/entites/systems/RenderSystem.java
+++ core/src/com/mygdx/game/entites/systems/RenderSystem.java
@@ -5,11 +5,14 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.mygdx.game.entites.entitiycomponents.*;
+import com.mygdx.game.utils.Assets;
 
 /**
  * Created by MichaelSjogren on 2017-03-04.
@@ -36,8 +39,9 @@ public class RenderSystem extends EntitySystem{
             StateComponent stateComp = entity.getComponent(StateComponent.class);
             DirectionComponent dirComp = entity.getComponent(DirectionComponent.class);
             HealthComponent healthComp = entity.getComponent(HealthComponent.class);
+            healthComp.health -= .1f;
             drawHealthbar(healthComp, posComp);
-            TextureRegion region = ((TextureRegion) animComp.animations.get(stateComp.get()).getKeyFrame(stateComp.time , true));
+            TextureRegion region =  (TextureRegion) animComp.animations.get(stateComp.get()).getKeyFrame(stateComp.time , true);
             batch.draw( region
                     ,posComp.x,posComp.y,region.getRegionWidth() / 2 , region.getRegionHeight() / 2
             ,region.getRegionWidth() ,region.getRegionHeight(),1,1,dirComp.angle);
@@ -45,17 +49,24 @@ public class RenderSystem extends EntitySystem{
     }
     
     private void drawHealthbar(HealthComponent healthComp , PositionComponent pos){
-    	float healthbarHeight = 1.2f;
-    	float healthbarWidth = 16;
+    	float healthbarHeight = Assets.enemyRedHealthbarBG.getHeight();
+    	float healthbarWidth = Assets.enemyRedHealthbarBG.getWidth();
     	// getting the scale ratio
     	float ratio = healthbarWidth / healthComp.maxHealth;
-    	float currentWidth = healthComp.health * ratio;
-		// red background
-        sr.set(ShapeType.Filled);
-        sr.setColor(1f, 0,0,1f);
-        sr.rect(pos.x + healthbarWidth / 2, pos.y , healthbarWidth , healthbarHeight);
-        // green current health
-        sr.setColor(0, 1f , 0 , 1f);
-        sr.rect(pos.x + healthbarWidth / 2, pos.y, currentWidth , healthbarHeight);
+    	Sprite spriteBg = Assets.enemyRedHealthbarBG;
+    	Sprite sprite  = Assets.enemyGreenHealthbarBG;
+    	sprite.setPosition(pos.x, pos.y);
+    	spriteBg.setPosition(pos.x, pos.y);
+    	sprite.setSize(ratio * healthComp.health , healthbarHeight);
+    	// 100 %
+    	if(healthComp.health <= 1 * healthComp.maxHealth) sprite.setColor(Color.GREEN);
+    	// 75 %
+    	if(healthComp.health <= 0.75 * healthComp.maxHealth) sprite.setColor(Color.YELLOW);
+    	// 50%
+    	if(healthComp.health <= 0.50 * healthComp.maxHealth) sprite.setColor(Color.ORANGE);
+    	// 25%
+    	if(healthComp.health <= 0.25 * healthComp.maxHealth) sprite.setColor(Color.RED);
+    	spriteBg.draw(batch);
+    	sprite.draw(batch);
     }
 }
