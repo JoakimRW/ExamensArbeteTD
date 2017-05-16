e446a001f0314f7a45d125d3c16cf99150bcdf24
michael sjögren <mikel-carl@hotmail.com>
2017-05-16 20:08:06 +0200

    pathfinding optimization

--- core/src/com/mygdx/game/utils/PathFinder.java
+++ core/src/com/mygdx/game/utils/PathFinder.java
@@ -36,8 +36,6 @@ public abstract class PathFinder {
                     path.add(current);
                     current = current.getParent();
                 }
-                openList.clear();
-                closedList.clear();
                 return path;
             }
             openList.remove(current);
@@ -53,6 +51,37 @@ public abstract class PathFinder {
                 }
                 int x = MathUtils.floor(current.getCordinates().x);
                 int y = MathUtils.floor(current.getCordinates().y);
+            
+                // at is a valid path
+                // to prevent creating a path diagonally we have to check
+                // that there arent any walls between the diagonal path
+
+                // when topleft(diagonally) is being considerd, check if it has any walls below it or to its right
+                // if it does, continue
+                if(canGoDiag){
+                    final Tile top = LevelManager.getTile(x  , y  - 1);
+                    final Tile bottom = LevelManager.getTile(x , y + 1);
+                    final Tile right = LevelManager.getTile(x  + 1 , y );
+                    final Tile left = LevelManager.getTile(x  - 1, y);
+                    
+                    if(i == 0){
+                        if(top == null || left == null) continue;
+                        if(top.getType() == TileType.WALL && left.getType() == TileType.WALL) continue;
+                    }
+                    if(i == 2){
+                        if(top == null || right == null) continue;
+                        if(top.getType() == TileType.WALL && right.getType() == TileType.WALL) continue;
+                    }
+                    if(i == 6){
+                        if(bottom == null || left == null) continue;
+                        if(bottom.getType() == TileType.WALL && left.getType() == TileType.WALL) continue;
+                    }
+                    if(i == 8){
+                        if(bottom == null || right == null) continue;
+                        if(bottom.getType() == TileType.WALL && right.getType() == TileType.WALL) continue;
+                    }
+                }
+                
                 int xDir = (i % 3) - 1;
                 int yDir = (i / 3) - 1;
                 Tile at = LevelManager.getTile(x + xDir , y + yDir);
@@ -60,31 +89,7 @@ public abstract class PathFinder {
                 if(!isFlying){
                 	if(at.getType() == TileType.WALL) continue;
                 }
-                // at is a valid path
-                // to prevent creating a path diagonally we have to check
-                // that there arent any walls between the diagonal path
-                final Tile top = LevelManager.getTile(x  , y  - 1);
-                final Tile bottom = LevelManager.getTile(x , y + 1);
-                final Tile right = LevelManager.getTile(x  + 1 , y );
-                final Tile left = LevelManager.getTile(x  - 1, y);
-                // when topleft(diagonally) is being considerd, check if it has any walls below it or to its right
-                // if it does, continue
-                if(i == 0){
-                    if(top == null || left == null) continue;
-                    if(top.getType() == TileType.WALL && left.getType() == TileType.WALL) continue;
-                }
-                if(i == 2){
-                    if(top == null || right == null) continue;
-                    if(top.getType() == TileType.WALL && right.getType() == TileType.WALL) continue;
-                }
-                if(i == 6){
-                    if(bottom == null || left == null) continue;
-                    if(bottom.getType() == TileType.WALL && left.getType() == TileType.WALL) continue;
-                }
-                if(i == 8){
-                    if(bottom == null || right == null) continue;
-                    if(bottom.getType() == TileType.WALL && right.getType() == TileType.WALL) continue;
-                }
+
                 Vector2 a = new Vector2(x + xDir , y + yDir);
                 double gCost = current.getgCost() + current.getCordinates().dst(a);
                 double hCost = current.getCordinates().dst(endNode);
@@ -93,8 +98,6 @@ public abstract class PathFinder {
                 if (!vectInList(openList, a) || gCost < current.getgCost()) openList.add(node);
             }
         }
-        closedList.clear();
-
 //        System.out.println("found no path, returning null :: START :: " + startNode + " END :: "  + endNode);
         return null;
     }
