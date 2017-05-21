ec894e4c8a75f1c64ed37bfd34414dc59a9a5c6a
michael sjögren <mikel-carl@hotmail.com>
2017-05-21 16:00:50 +0200

    win stage , state

--- core/src/com/mygdx/game/stages/WinStage.java
+++ core/src/com/mygdx/game/stages/WinStage.java
@@ -1,7 +1,45 @@
 package com.mygdx.game.stages;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.game.utils.Assets;
 
 public class WinStage extends Stage {
+	
+	private TextButton playAgainBtn;
+	private TextButton mainMenuBtn;
+	
+	public WinStage(){
+		super();
+		initWinStage();
+	}
 
+	private void initWinStage() {
+		getCamera().position.set(Gdx.graphics.getWidth() / 2 , Gdx.graphics.getHeight() / 2 , 0);
+		getCamera().update();
+		Label youWinLabel = new Label("You Won!" , Assets.mainMenuSkin);
+		playAgainBtn = new TextButton("Play Again", Assets.mainMenuSkin);
+		mainMenuBtn = new TextButton("Main Menu", Assets.mainMenuSkin);
+		Table container = new Table(Assets.mainMenuSkin);
+		youWinLabel.setColor(Color.GREEN);
+		container.add(youWinLabel).spaceBottom(100).row();
+		container.add(playAgainBtn).align(Align.left).spaceBottom(20).row();
+		container.add(mainMenuBtn).align(Align.left);
+		container.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		addActor(container);
+	}
+
+	public TextButton getPlayAgainBtn() {
+		// TODO Auto-generated method stub
+		return playAgainBtn;
+	}
+	
+	public TextButton getMainMenuBtn(){
+		return mainMenuBtn;
+	}
 }
--- core/src/com/mygdx/game/states/WinState.java
+++ core/src/com/mygdx/game/states/WinState.java
@@ -1,47 +1,64 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.managers.GameStateManager;
+import com.mygdx.game.managers.GameStateManager.State;
+import com.mygdx.game.stages.WinStage;
 
 public class WinState extends GameState {
-
+	WinStage winStage;
 	public WinState(GameStateManager gsm) {
 		super(gsm);
-		// TODO Auto-generated constructor stub
+		winStage = new WinStage();
+		
+		Gdx.input.setInputProcessor(winStage);
+		
+		winStage.getPlayAgainBtn().addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				_gsm.setState(State.PLAY);
+			}
+		});
+		
+		winStage.getMainMenuBtn().addListener(new ClickListener(){
+			 @Override
+			public void clicked(InputEvent event, float x, float y) {
+				 _gsm.setState(State.MAINMENU);
+			}
+		});
 	}
 
 	@Override
 	public void resize(int w, int h) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void update(float delta) {
-		// TODO Auto-generated method stub
-
+		winStage.act();
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
-
+		winStage.draw();
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
+		winStage.dispose();
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
+	
 
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
+	
 
 	}
 
